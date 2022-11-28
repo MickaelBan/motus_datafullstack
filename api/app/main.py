939830c6766d4c14ed876eb78053cb6798031895
Mickaël Bannholtzer <bannholtzer.mickael@gmail.com>
2022-11-28 19:54:55 +0100

    coCORSMiddleware

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import BaseSQL,engine
 from routers import UserRouter,WordRouter
 from typing import Optional
@@ -9,6 +11,21 @@ app = FastAPI(
     description="My description",
     version="0.0.1",
 )
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(UserRouter)
 app.include_router(WordRouter)
